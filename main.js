1904a7089ebfcd7202f401be004effb1505a993e
Johannes Schweig <johannes.schweig@valsight.com>
2019-12-02 16:22:56 +0100

    Multi selection is supported
    added todo.md

--- main.js
+++ main.js
@@ -1,27 +1,19 @@
-//const { alert, error } = require("./lib/dialogs.js");
 const {Color} = require("scenegraph");
 
+// fill all elements in selection with a color
+function fill(selection, color) {
+  let itemsNum = selection.items.length;
+  for (let i = 0; i < itemsNum; i++) {
+    selection.items[i].fill = color;
+  }
+}
+
 function fillBlack(selection) {
-  //let itemsNum = selection.items.length;
-
-  //if(!itemsNum) {
-  //  error("Ooops!", "You need to select at least one layer.");
-  //  return;
-  //}
-
-  //for (let i = 0; i < itemsNum; i++) {
-  //  let border = selection.items[i].stroke;
-  //  let fill = selection.items[i].fill;
-  //  if(border && fill) {
-  //    selection.items[i].fill = border;
-  //    selection.items[i].stroke = fill;
-  //  }
-  //}
-  selection.items[0].fill = new Color("#000000");
+  fill(selection, new Color("#000000"));
 }
 
 function fillWhite(selection) {
-  selection.items[0].fill = new Color("#ffffff");
+  fill(selection, new Color("#ffffff"));
 }
 
 module.exports = {
---
+++ todo.md
@@ -0,0 +1,5 @@
+# TODO
+- fill layer should be enabled
+
+# DONE
+- add support for multiple selections (v0.02)
