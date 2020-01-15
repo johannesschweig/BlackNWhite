a13a4006a74328d2dfb33f4e938bf8b9e7fde364
Johannes Schweig <johannes.schweig@valsight.com>
2020-01-15 10:44:24 +0100

    Groups are supported
    - fill gets automatically enabled

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 const {Color} = require("scenegraph");
 
+// find all elements in a selection
+function findElements(array) {
+  let elements = []
+  array.forEach(function (e) {
+    if (e.constructor.name === "Group") {
+      let a = findElements(e.children)
+      elements = elements.concat(a)
+    } else {
+      elements.push(e)
+    }
+  })
+  return elements
+}
+
 // fill all elements in selection with a color
 function fill(selection, color) {
-  let itemsNum = selection.items.length;
-  for (let i = 0; i < itemsNum; i++) {
-    selection.items[i].fill = color;
+  let elements = findElements(selection.items)
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].fill = color;
+    elements[i].fillEnabled = true;
   }
 }
 
--- todo.md
+++ todo.md
@@ -1,5 +1,7 @@
 # TODO
-- fill layer should be enabled
+- add shortcuts for toggling fill/border on/off
 
 # DONE
+- added support for elements in groups (v0.03)
+- fill layer should be enabled (v0.03)
 - add support for multiple selections (v0.02)
